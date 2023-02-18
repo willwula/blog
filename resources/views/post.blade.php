51f0a58979846d923efbee4fc56cf5d97ff3e656
willwula <wfh318@gmail.com>
2023-02-18 12:36:26 +0800

    feat: "make a route and link" Laravel 8 From Scratch:S2-ep7

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis delectus ea enim ex facilis id
+            itaque maxime molestias nulla pariatur, porro quas quod suscipit totam unde ut voluptatum! Amet, doloribus?
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
