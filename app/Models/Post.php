3d69dbc3b2fa625d63356379bea5c17652394e30
willwula <wfh318@gmail.com>
2023-02-22 10:53:01 +0800

    feat: "A Few Tweaks and Consideration" Laravel 8 From Scratch:S3-ep16

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -50,4 +50,14 @@ class Post
 //        return cache()->remember("posts.{$slug}",1200,fn() => file_get_contents($path));
         return static::all()->firstWhere('slug',$slug);
     }
+
+    public static function findOrFail($slug)
+    {
+        $post =  static::find($slug);
+
+        if (! $post) {
+            throw new ModelNotFoundException();
+        }
+        return $post;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -41,8 +41,7 @@ Route::get('/', function () {
 Route::get('posts/{post}', function ($slug) {
     //Find a post by its slug and pass it to a view called "post"
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => Post::findOrFail($slug)
     ]);
-
-})->where('post', '[A-z_\-]+');
+});
 //whereAlpha('post')
