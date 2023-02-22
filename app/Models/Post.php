a30bd104c3da69077aa9933456ae740931ba0eb8
willwula <wfh318@gmail.com>
2023-02-22 16:18:30 +0800

    feat: "Make a Post Model and Migration" Laravel 8 From Scratch:S4-ep20

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,62 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public $title;
-    public $excerpt;
-    public $date;
-    public $body;
-    public $slug;
-
-    public function __construct($title, $excerpt, $date, $body, $slug)
-    {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all()
-    {
-        return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path("posts")))
-                ->map(fn($file) => YamlFrontMatter::parseFile($file))
-                ->map(fn($document) => new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-                ))
-                ->sortByDesc('date');
-        });
-        //File::\Illuminate\Support\Facades\ 尋找所有post
-//        $files =  File::files(resource_path("posts/"));
-//
-//        return array_map(fn($file) => $file->getContents(), $files);
-    }
-    public static function find($slug)
-    {
-//        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-//           throw new ModelNotFoundException();
-//        }
-//        return cache()->remember("posts.{$slug}",1200,fn() => file_get_contents($path));
-        return static::all()->firstWhere('slug',$slug);
-    }
-
-    public static function findOrFail($slug)
-    {
-        $post =  static::find($slug);
-
-        if (! $post) {
-            throw new ModelNotFoundException();
-        }
-        return $post;
-    }
+    use HasFactory;
 }
