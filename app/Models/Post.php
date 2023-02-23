571ea168c5911768188dcfdf26c9b17e82806d2c
willwula <wfh318@gmail.com>
2023-02-23 15:29:29 +0800

    feat: "Your First Eloquent Relationship" Laravel 8 From Scratch:S4-ep24

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,10 @@ class Post extends Model
 
     protected $guarded = [];
 
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
 //    public function getRouteKeyName()
 //    {
 //        return 'slug';
