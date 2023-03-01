fa8cf7fae454faba361c9921d3d5035443c5c6ed
willwula <wfh318@gmail.com>
2023-03-01 17:12:31 +0800

    feat: "Convert the Blog Post Page" Laravel 8 From Scratch:S5-ep33

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
-            <x-post-grid :post="$posts" />
+            <x-post-grid :posts="$posts" />
         @else
             <p class="text-center">no posts yet. Please check back later.</p>
         @endif
