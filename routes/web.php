00ab41b8a79e9b895a1e629ec089c9b8b6da478a
willwula <wfh318@gmail.com>
2023-03-02 12:56:54 +0800

    feat: "A Small JavaScript Dropdown Detour" Laravel 8 From Scratch:S5-ep34

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,8 @@ Route::get('/', function () {
 //        );
 //        ddd($document->matter('title'));
     return view('posts', [
-        'posts' => Post::latest()->with(['category', 'author'])->get()
+        'posts' => Post::latest()->with(['category', 'author'])->get(),
+        'categories' => Category::All()
     ]);
 });
 
@@ -50,12 +51,15 @@ Route::get('posts/{post:slug}', function (Post $post) {
 //whereAlpha('post')
 Route::get('/categories/{category:slug}',function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
         ]);
 });
 
 Route::get('/authors/{author:username}',function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
