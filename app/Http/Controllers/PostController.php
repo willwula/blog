b27591b3bdd3cfd06ce266b6d21b43e709fc6189
willwula <wfh318@gmail.com>
2023-03-03 17:10:15 +0800

    feat: "Extract a Category Dropdown Blade Component" Laravel 8 From Scratch:S7-ep40

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,22 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search']))->get(),
-            'categories' => Category::All(),
-            'currentCategory' => Category::firstWhere('slug', request('category'))
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category']))->get()
         ]);
        }
 
        public function show(Post $post) {
-        return view('post', [
+        return view('posts.show', [
                'post' => $post
            ]);
        }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    @include('_posts-header')
+    @include('posts._header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
@@ -7,14 +7,9 @@
         @else
             <p class="text-center">no posts yet. Please check back later.</p>
         @endif
-{{--        <div class="lg:grid lg:grid-cols-3">--}}
-{{--            <x-post-card />--}}
-{{--            <x-post-card />--}}
-{{--            <x-post-card />--}}
 
-
-{{--        </div>--}}
     </main>
+
 {{--    @foreach ($posts as $post )--}}
 {{--        <article>--}}
 {{--            <h1>--}}
@@ -32,4 +27,5 @@
 {{--            </div>--}}
 {{--        </article>--}}
 {{--    @endforeach--}}
+
 </x-layout>
