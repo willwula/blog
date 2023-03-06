15cd4fc330c5e021a1cd46708ebc181b7d57fb2f
willwula <wfh318@gmail.com>
2023-03-06 14:06:03 +0800

    feat: "Laughably Simple Pagination" Laravel 8 From Scratch:S8-ep44

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,11 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index() {
+
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get()
+            'posts' => Post::latest()->filter(
+                request(['search', 'category', 'author'])
+            )->paginate(6)->withQueryString()
         ]);
        }
 
