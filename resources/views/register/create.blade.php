327f6c49bc7d9901ad9eea7e19cb2fa111006f55
willwula <wfh318@gmail.com>
2023-03-09 17:24:29 +0800

    feat: "Build a Register User Page" Laravel 8 From Scratch:S9-ep45

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,42 @@
+<x-layout>
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
+            <h1 class="text-center font-bold text-xl">Register!</h1>
+
+            <form method="post" action="/register" class="mt-10">
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="name"
+                    >
+                        Name
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="text"
+                           name="name"
+                           id="name"
+                           required
+                    >
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="username"
+                           >
+                        Username
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="text"
+                           name="username"
+                           id="username"
+                           required
+                    >
+                </div>
+
+
+            </form>
+        </main>
+    </section>
+</x-layout>
